6ff82a634ffdf2e0b46337795865fa117c87e4d8
christian quiles <quiles.11@osu.edu>
2023-07-06 00:46:14 +0000

    script

---
+++ R_LinearRegression_model_scatter.r
@@ -0,0 +1,20 @@
+library(ggplot2)
+
+filename = "regrex1.csv"
+
+dataset = read.csv(filename)
+
+dataset
+
+model = lm(formula = y~x, data=dataset)
+
+ggplot() +
+  geom_point(aes(x = dataset$x, y = dataset$y),
+             colour = 'red') +
+  ggtitle('y vs x') +
+  xlab('x') +
+  ylab('y')
+
+ggsave("R_linear_regression_scatter.png")
+
+dev.off()
